f6deb84e4b04d24400f4eb7e2527d8dc82d28b84
David Håkansson <davidhak@live.se>
2024-03-25 18:10:56 +0100

    Lite depends

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
