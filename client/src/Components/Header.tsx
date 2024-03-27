a81716c5c16cd7d4d722a8980cca7118b9df7f55
Emma Jansson <emma.m.jansson1@gmail.com>
2024-03-27 12:39:26 +0100

    emma och lina testar

---
+++ client/src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import App from '../App';
+
+
+    const Header: React.FC = () => {
+        return (
+            <div className='header'>
+                    <h1>STUDENTGUIDEN!!!1</h1>
+            </div>
+        );
+    }
+    
+export default Header;
