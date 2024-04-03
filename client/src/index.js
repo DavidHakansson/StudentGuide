9f0e099f6514fdd331819c8ee28e3d526c35a2d9
EffPeeEl <65282480+EffPeeEl@users.noreply.github.com>
2024-04-03 11:01:44 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ root.render(
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-5TL155XBJ9"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments)}
+      function gtag(){dataLayer.push(arguments;)}
       gtag('js', new Date());
 
       gtag('config', 'G-5TL155XBJ9');
