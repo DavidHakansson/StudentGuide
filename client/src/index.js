cc3dc4f9cdbe9e7f7edc64065a7b314cf6d7ec58
EffPeeEl <65282480+EffPeeEl@users.noreply.github.com>
2024-04-03 10:54:13 +0200

    added G-Tags

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5TL155XBJ9"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-5TL155XBJ9');
+    </script>
+    
     <link
       rel="stylesheet"
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
