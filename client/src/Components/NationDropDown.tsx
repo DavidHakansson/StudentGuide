c9c0b93778a010231edef1632daf92237b29e5f2
LinaRasmusson <lina.rasmusson@gmail.com>
2024-04-03 15:10:24 +0200

    eventsInformationUpdated

--- client/src/Components/NationDropDown.tsx
+++ client/src/Components/NationDropDown.tsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect, ChangeEvent } from "react";
 //import EmojiIcon from '@atlaskit/icon/glyph/emoji';
 import Select, { components, DropdownIndicatorProps } from "react-select";
 //import { ColourOption, colourOptions } from '../data';
-import '../Styling/NationDropDown.css'; 
-import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'; // Assuming Bootstrap 4 theme for PrimeReact
-import 'primereact/resources/primereact.min.css'; // Core CSS
-
+import "../Styling/NationDropDown.css";
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css"; // Assuming Bootstrap 4 theme for PrimeReact
+import "primereact/resources/primereact.min.css"; // Core CSS
 
 import {
   EventNationOption,
@@ -13,14 +12,11 @@ import {
   DefaultNationOptions,
 } from "./Nations";
 
-import { MultiSelect } from 'primereact/multiselect';
-
+import { MultiSelect } from "primereact/multiselect";
 
 interface NationDropDownProps {
   onChange: (nations: string[]) => void;
 }
-
-
 
 const DropdownIndicator = (
   props: DropdownIndicatorProps<EventNationOption, true>
@@ -38,21 +34,18 @@ const NationDropDown: React.FC<NationDropDownProps> = ({ onChange }) => {
   }, [nations, onChange]);
 
   return (
-    
     <div className="flex justify-content-center">
-      <MultiSelect value={nations} onChange={(e) => setNations(e.value)} 
-      options={DefaultNationOptions} 
-      optionLabel="label" 
-      maxSelectedLabels={3} 
-      placeholder="Select Nations"
-      className="w-full md:w-20rem"
-      selectedItemsLabel="{0} nations selected"
+      <MultiSelect
+        value={nations}
+        onChange={(e) => setNations(e.value)}
+        options={DefaultNationOptions}
+        optionLabel="label"
+        maxSelectedLabels={3}
+        placeholder="Select Nations"
+        className="w-full md:w-20rem"
+        selectedItemsLabel="{0} nations selected"
       />
-      
     </div>
-    
-
-
   );
 };
 export default NationDropDown;
