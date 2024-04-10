57287aa4894c21e2938f3b2228ece6b9504352af
David Håkansson <davidhak@live.se>
2024-04-10 13:09:01 +0200

    added logo and changed colours

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,33 +1,42 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const Header: React.FC = () => {
-    const [expanded, setExpanded] = useState(false);
+const headerStyle = {
+  color: 'black', // Example style for the Navbar.Brand
+  // Add more styles as needed
+};
 
-    const toggleExpanded = () => {
-        setExpanded(!expanded);
-    };
+const NavigationBar = () => {
+  const [expanded, setExpanded] = useState(false);
 
-    const headerStyle = {
-        fontSize: '2rem' // Adjust the font size here as needed
-    };
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
 
-    return (
-        <div className='container'>
-        <Navbar bg="light" expand="lg" expanded={expanded}>
-            <Navbar.Brand as={Link} to="/" style={headerStyle}>STUGGEN</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleExpanded} />
-            <Navbar.Collapse id="basic-navbar-nav" className={`${expanded ? 'show' : ''}`}>
-                <Nav className="ml-auto">
-                    <Nav.Link as={Link} to="/" onClick={toggleExpanded}>Home</Nav.Link>
-                    <Nav.Link as={Link} to="/Valborg" onClick={toggleExpanded}>Valborg</Nav.Link>
-                    {/* Add more Nav.Link components for additional menu items */}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        </div>
-    );
-}
+  return (
+    <div className='container'>
+      <Navbar bg="light" expand="lg" expanded={expanded}>
+        {/* Replace Navbar.Brand with Image component */}
+        <Navbar.Brand as={Link} to="/" onClick={toggleExpanded}>
+          <img
+            src={'/logo_alt2.png'}
+            width="140"
+            className="d-inline-block align-top"
+            alt="STUGGEN"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleExpanded} />
+        <Navbar.Collapse id="basic-navbar-nav" className={`${expanded ? 'show' : ''}`}>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/" onClick={toggleExpanded}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/Valborg" onClick={toggleExpanded}>Valborg</Nav.Link>
+            {/* Add more Nav.Link components for additional menu items */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
-export default Header;
+export default NavigationBar;
